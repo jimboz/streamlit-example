026f425157272a46f7b8b621d4b3d1e9dc26e905
jimboz <jim.basilakis@gmail.com>
2023-08-19 10:30:22 +1000

    new game

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,17 +8,77 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
-def draw_circle(radius):
-    fig, ax = plt.subplots(figsize=(6, 6))
-    circle = plt.Circle((0, 0), radius, color='m', fill=False)
-    ax.add_artist(circle)
-    ax.set_xlim(-radius-1, radius+1)
-    ax.set_ylim(-radius-1, radius+1)
-    ax.set_aspect('equal', 'box')
-    ax.grid(True, which='both', linestyle='--', linewidth=0.5)
-    ax.axhline(y=0, color='k', linestyle='--', linewidth=0.5)
-    ax.axvline(x=0, color='k', linestyle='--', linewidth=0.5)
-    st.pyplot(fig)
+
+# Maze Dimensions
+ROWS, COLS = 10, 10
+
+# Directions for movement
+DIRECTIONS = {
+    "UP": (-1, 0),
+    "DOWN": (1, 0),
+    "LEFT": (0, -1),
+    "RIGHT": (0, 1)
+}
+
+# Initialize maze with all walls
+maze = np.ones((ROWS, COLS), dtype=int)
+
+# Start and End points
+start, end = (1, 1), (ROWS - 2, COLS - 2)
+
+# Create a path (For simplicity, we're creating a straight path)
+for i in range(1, ROWS-1):
+    maze[i][1:COLS-1] = 0
+
+maze[end] = 2  # Mark the end point
+
+player_position = list(start)
+
+
+def draw_maze(maze, position):
+    """Display the maze with the player's current position."""
+    display = maze.copy()
+    display[position[0], position[1]] = 3
+    st.image(display, caption="Maze", width=300, channels="GRAY")
+
+
+def move(direction, position):
+    """Move the player in the given direction."""
+    new_position = list(position)
+    move = DIRECTIONS[direction]
+    new_position[0] += move[0]
+    new_position[1] += move[1]
+
+    # Check for walls
+    if maze[new_position[0], new_position[1]] == 1:
+        return position
+    return tuple(new_position)
+
+
+st.title("Maze Puzzle")
+
+draw_maze(maze, player_position)
+
+move_direction = st.selectbox("Choose a direction", list(DIRECTIONS.keys()))
+
+player_position = move(move_direction, player_position)
+
+if player_position == end:
+    st.success("You've reached the end!")
+else:
+    st.write("Move to reach the end!")
+
+# def draw_circle(radius):
+#     fig, ax = plt.subplots(figsize=(6, 6))
+#     circle = plt.Circle((0, 0), radius, color='m', fill=False)
+#     ax.add_artist(circle)
+#     ax.set_xlim(-radius-1, radius+1)
+#     ax.set_ylim(-radius-1, radius+1)
+#     ax.set_aspect('equal', 'box')
+#     ax.grid(True, which='both', linestyle='--', linewidth=0.5)
+#     ax.axhline(y=0, color='k', linestyle='--', linewidth=0.5)
+#     ax.axvline(x=0, color='k', linestyle='--', linewidth=0.5)
+#     st.pyplot(fig)
 
 """
 # Welcome to Streamlit!
@@ -31,10 +91,10 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-with st.echo(code_location='below'):
-    st.title("Draw a Circle")
-    radius = st.slider("Select a radius:", 1, 10, 5)
-    draw_circle(radius)
+# with st.echo(code_location='below'):
+#     st.title("Draw a Circle")
+#     radius = st.slider("Select a radius:", 1, 10, 5)
+#     draw_circle(radius)
     
 # with st.echo(code_location='below'):
 #     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
