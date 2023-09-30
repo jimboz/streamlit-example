bdfa67d98c4a1e237b3f02c0f936b64c18c6a89d
jimboz <jim.basilakis@gmail.com>
2023-09-30 14:37:40 +1000

    print maze

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,7 @@ player_position = list(start)
 def draw_maze(maze, position):
     """Display the maze with the player's current position."""
     display = maze.copy()
+    print(maze)
     # display[position[0], position[1]] = 3
     st.image(display, caption="Maze", width=300, channels="GRAY")
 
